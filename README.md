68c8fdd505c2fd05e414e98137e6e157255cbf20
Lavie K <laviekolchinsky@gmail.com>
2019-01-19 00:51:27 +1100

    Changed many features and cleaned up the code. Added a ping, echo, clear, auto-role, auto-printing deleted messages commands and functionalities.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # creepyindianguy.py
 A memeulous Discord bot. Based on posts from r/indianpeoplefacebook. 
+
+Requirements: Python 3 and Discord.
+
+`pip3 install discord`
+`import discord`
+`import ext.commands from discord`
--- creepyindianguy.py
+++ creepyindianguy.py
@@ -1,34 +1,59 @@
 import discord
+import discord.game
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+from itertools import cycle
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "bob.")
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name=" bitch lasagna")) #make bitch lasagna the help command
     print("Bot is ready for kiss bob!")
 
 @client.event
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-    mhs_guild = client.get_guild(530704685708083201)
+    await client.process_commands(message)
 
-    if "indianguy.logout" == message.content.lower():
-        await message.channel.send("'''py\n{0}'''".format(mhs_guild.member_count))
-
-    elif "bob" in message.content.lower():
-        await client.send_message(message.channel, "Did somebody say bobs? I'M KISS BOBS!!!")
-
-    elif message.content.lower() == "indianguy.logout":
+    if message.content.lower() == "indianguy.logout":
         await client.close()
 
-#Why doesn't this work? How can I get it to work?
-    # elif message.content.lower() == "indianguy.login":
-       ## await client.open()
+#We don't really need this on-delete functionality if we have logger, unless we can replace it. Still useful in general.
+#@client.event
+# sync def on_message_delete(message):
+    #await client.send_message(message.channel, f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+    #await client.process_commands(message)
+
+#MISSING PERMISSIONS ERROR @client.event
+#async def on_member_join(member):
+    #role = discord.utils.get(member.server.roles, name='W1GG3R5')
+    #await client.add_roles(member, role)
+
+@client.command()
+async def ping():
+    await client.say('Pong!')
+
+@client.command()
+async def echo(*args):
+    output = ''
+    for word in args:
+        output += word
+        output += ' '
+    await client.say(output)
+
+@client.command(pass_context=True)
+async def clear(ctx, amount=10):
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit=int(amount)):
+        messages.append(message)
+    await client.delete_messages(messages)
+    await client.say('Messages deleted.')
+
+
 
 client.run("NTMxMDQ1ODcxNDUwNzE4MjA5.DyCTow.LRGTi9OizIe1E0STFRCFmrOSJV0")
-
-#We need a help function (e.g. ".bob -help")
